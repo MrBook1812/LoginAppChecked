4f539182f1ac0f6981cfcbb6e5c58d75bf96fafc
Kirill Kniga <kiruha_95@icloud.com>
2023-02-14 17:02:11 +0300

    Work with segue

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+final class LoginViewController: UIViewController {
+    @IBOutlet var userNameLabel: UITextField!
+    @IBOutlet var passwordLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? WelcomeViewController else { return }
+        settingsVC.welcomeUser = userNameLabel.text
+    }
+    
+    
+    @IBAction func logInButtonTapped() {
+    }
+    
+    @IBAction func forgotUserNameButtonTapped() {
+        showUserName(with: "Your Log In is:", andmessage: "Alex")
+    }
+    
+    @IBAction func forgotPasswordButtonTapped() {
+        showPass(with: "Your Password is:", andmessage: "1")
+    }
 }
 
+// MARK: - UIAlertController
+extension LoginViewController {
+    private func showUserName(with title: String, andmessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    private func showPass(with title: String, andmessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Oh, yes!", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
