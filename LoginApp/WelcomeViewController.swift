44b0018bf2dd6c7847449b81ccd83fde41b7f477
Kirill Kniga <kiruha_95@icloud.com>
2023-02-14 19:08:14 +0300

    Gradient

--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -13,17 +13,26 @@ final class WelcomeViewController: UIViewController {
     @IBOutlet var hiLabel: UILabel!
     
     var welcomeUser: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         welocomeLabel.text = "Hello, \(String(welcomeUser))"
         hiLabel.text = "👋"
+        setGradientBackground()
+
+    }
+    
+    func setGradientBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.systemGreen.cgColor, UIColor.blue.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.3, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.3, y: 0.0)
+        gradientLayer.locations = [0,1]
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
     @IBAction func logOutButtonTapped() {
         dismiss(animated: true)
-        
     }
-    
-    
 }
